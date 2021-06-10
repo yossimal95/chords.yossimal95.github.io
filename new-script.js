35063ed3e077dc7f3e2e8279be2e55c33919fdca
yossimal95 <70268960+yossimal95@users.noreply.github.com>
2021-06-10 14:16:59 +0300

    Add files via upload

---
+++ new-script.js
@@ -0,0 +1,86 @@
+// 0 - chords list
+//var chordsList = [];
+var chordsList = 'Am,A,A7,B,Bm,B7,Bb,C,C7,Dm,D,D7,E,Em,E7,F,F7,G,Gm,G7';
+
+const loadChords = () => {
+    let temp = chordsList.split(',');
+    for (let index = 0; index < temp.length; index++) {
+        document.getElementById("chordsSelect").innerHTML += `<option value="` + temp[index] + `">` + temp[index] + `</option>`
+       
+    }
+}
+
+loadChords();
+
+// 1 - get the song words
+const orgenizeTheWords = () => {
+    // textarea with words
+    let ta = document.getElementById("wordsTa");
+    // if the TA is empty => return;
+    if (ta.value.trim() == '') {
+        return;
+    }
+    // get the output board div
+    let board = document.getElementById("cordsBoard");
+    // load the words inside the board
+    board.innerHTML = splitTheWords(ta.value.replaceAll('\n','br '));
+}
+
+// 2 - 
+const splitTheWords = (str) => {
+    let wordsArr = str.split(' ');
+    let res = '';
+    for (let index = 0; index < wordsArr.length; index++) {        
+        res += addSpan(wordsArr[index])
+    }
+    return res;
+} 
+
+// 3 -
+const addSpan= (str) => {
+    if (str.indexOf('br') > -1) {        
+        return '<span class="word" onclick="choosChorde(this)">' + str.replaceAll('br','</span>' + addBlankWords(4)) + addBlankWords(1) + '<br>';
+    }
+    return '<span class="word" onclick="choosChorde(this)">' + str + '</span>' + addBlankWords(2);
+}
+
+// 4 - 
+const addBlankWords = (numOfBlankWords) => {
+    let res = '';
+    for (let index = 0; index < numOfBlankWords; index++) {
+        res += '<span class="word" onclick="choosChorde(this)">&nbsp;&nbsp;&nbsp;</span>';  
+    }
+    return res;
+}
+
+// 5 -
+var chosenElement;
+
+// 6 - 
+const choosChorde = (element) => {
+    if (element.innerText.trim() == '') {
+        element.innerHTML += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+    }
+    chosenElement = element;
+
+}
+
+// 7 -
+const ok = () => {
+    let select = document.getElementById("chordsSelect");
+    let chord = select[select.selectedIndex].value;
+    chosenElement.classList.add("chordClass"); 
+    chosenElement.setAttribute("chord", chord);
+}
+
+function browse(tabId) {
+    let x = document.querySelectorAll('.tabs');
+    for (let index = 0; index < x.length; index++) {
+        x[index].style.display = 'none';
+    }
+    document.getElementById(tabId).style.display = 'block';
+
+    if (tabId == 'chords') {
+        orgenizeTheWords();
+    }
+}
